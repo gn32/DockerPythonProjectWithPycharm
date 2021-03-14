5a2fe5f79e919a9fd1beaeee4e42ef078239d34c
garima negi <gn32@njit.edu>
2021-03-14 15:44:02 -0400

    Feature : Created new python script for multiplying two numbers

---
+++ my_script.py
@@ -0,0 +1,9 @@
+#Python program to multiply two numbers using function
+
+def mul_num(a,b):#function for find product
+    multiply=a*b
+    return multiply #return value
+
+num1=25  #variable declaration
+num2=55
+print("The product is",mul_num(num1,num2))#call the function
